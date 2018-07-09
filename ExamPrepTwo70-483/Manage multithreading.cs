1ff5e28dbfaadb072f3e074cdd8a290d5f3bbfd1
conrellreddick <admin@dev.lawgen.org>
2018-07-09 13:12:38 -0400

    Add a Custome event accessor

--- ExamPrepTwo70-483/Manage multithreading.cs
+++ ExamPrepTwo70-483/Manage multithreading.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace ExamPrepTwo70_483
@@ -27,5 +28,99 @@ namespace ExamPrepTwo70_483
         //    calc = Multiply;
         //    Console.WriteLine(calc(3, 4));
         //}
+
+        //multicast delegate 
+
+        //public void MethodOne()
+        //{
+        //    Console.WriteLine("MethodOne");
+        //}
+
+        //public void Methodtwo()
+        //{
+        //    Console.WriteLine("MethodTwo");
+        //}
+
+        //public delegate void Del();
+
+        //public void Multicast()
+        //{
+        //    Del d = MethodOne;
+        //    d += Methodtwo;
+        //    d();
+        //}
+
+        //Convariance with delegates
+
+        // Lambda expression to create a delegate
+
+        // Using the Action delegate
+
+        //Action<int, int> calc = (x, y) => 
+        //{
+        //    Console.WriteLine(x + y);
+        //};
+
+        //calc(3,4); 
+
+
+
     }
+
+    //Using an Action to expose an event
+    //public class Pub
+    //{
+    //    public Action OnChange { get; set; }
+
+    //    public void Raise()
+    //    {
+    //        if (OnChange != null)
+    //        {
+    //            OnChange();
+    //        }
+    //    }
+    //}
+    //public void CreateAndRaise()
+    //{
+    //    Pub p = new Pub();
+    //    p.OnChange += () => Console.WriteLine("Event raised to method 1");
+    //    p.OnChange += () => Console.WriteLine("Event raise to method 2");
+    //    p.Raise();
+    //}
+    //public event Action OnChange = delegate { };
+
+    //public void Raise()
+    //{
+    //    OnChange();
+    
+    //Custom event arguments
+
+    //public class MyArgs : EventArgs
+    //{
+    //    public int Value { get; set; }
+
+    //    public MyArgs(int value)
+    //    {
+    //        Value = value;
+    //    }
+    //}
+
+    //public class Pub
+    //{
+    //    public event EventHandler<MyArgs> OnChange = delegate { };
+
+    //    public void Raise()
+    //    {
+    //        OnChange(this, new MyArgs(42));
+    //    }
+    //}
+
+    //public void CreateAndRaise()
+    //{
+    //    Pub p = new Pub();
+    //    p.OnChange += (sender, e) => Console.WriteLine("Event raised: {0}", e.Value);
+
+    //    p.Raise();
+
+    //}
 }
