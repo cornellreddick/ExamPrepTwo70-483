18e5ad7c5c7b306a948dd2b245b1f1727967083c
conrellreddick <cornellreddick@outlook.com>
2018-07-23 15:28:11 -0400

    Using a where clause on a class definition

---
+++ ExamPrepTwo70-483/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamPrepTwo70_483
+{
+    //using a where clause on a class definition
+    class MyClass<T> where T : class, new()
+    {
+        public MyClass()
+        {
+            MyProperty = new T();
+        }
+
+        T MyProperty { get; set; }
+    }
+}
