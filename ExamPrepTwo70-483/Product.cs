ea66ae37b27098776fb2004ef5b87b988ebf0e61
conrellreddick <cornellreddick@outlook.com>
2018-07-23 15:38:54 -0400

    Creating an extention method

---
+++ ExamPrepTwo70-483/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamPrepTwo70_483
+{
+    public class Product
+    {
+        //Creating an extension method
+        public decimal Price { get; set;  }
+    }
+
+    public static class MyExtensions
+    {
+        public static decimal Discount(this Product product)
+        {
+            return product.Price * .9M; 
+        }
+    }
+
+    public class Calculator
+    {
+        public decimal CalculateDiscount(Product p)
+        {
+            return p.Discount();
+        }
+    }
+}
