98285c5d451b269e93f0e286dd471c956d40f8ce
conrellreddick <cornellreddick@outlook.com>
2018-07-24 12:17:47 -0400

    Chaning constructors

---
+++ ExamPrepTwo70-483/Base.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamPrepTwo70_483
+{//Overriding a virtual method
+
+    class Base
+    {
+        public virtual int MyMethod()
+        {
+            return 42;
+        }
+    }
+
+    class Derived : Base
+    {
+        public sealed override int MyMethod() //Using the sealed keyword on a method 
+        {
+            return base.MyMethod() * 2;
+                
+        }
+    }
+    class Derived2 : Derived
+    {
+        // This line would give a compile error
+        //public override int MyMethod() { return 1; }
+    }
+}
+
---
+++ ExamPrepTwo70-483/ConstructorChaining.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamPrepTwo70_483
+{
+    class ConstructorChaining
+    {
+        //Chaining constructors
+        private int _p;
+
+        public ConstructorChaining() : this(3)
+        {
+
+        }
+        public ConstructorChaining(int p)
+        {
+            this._p = p; 
+        }
+
+           
+    }
+}
