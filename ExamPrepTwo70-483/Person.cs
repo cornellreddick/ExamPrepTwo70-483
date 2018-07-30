8fc2f23e62deefcfca1d1a955d22e2e0b3ad72fa
conrellreddick <cornellreddick@outlook.com>
2018-07-30 12:23:18 -0400

    Implement IEnumberalbe<T> on a custom type

---
+++ ExamPrepTwo70-483/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamPrepTwo70_483
+{
+    class People
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public People(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+
+
+    }
+
+    class Human : IEnumerable<People>
+    {
+        People[] people;
+
+        public Human(People[] people)
+        {
+            this.people = people;
+        }
+
+        public IEnumerator<People> GetEnumerator()
+        {
+            for (int index = 0; index < people.Length; index++)
+            {
+                yield return people[index];
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
