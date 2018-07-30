b9adb89bf60f9b07297df471ff9871c69c147c90
conrellreddick <cornellreddick@outlook.com>
2018-07-30 13:17:33 -0400

    Working with attributes

--- ExamPrepTwo70-483/CategoryAttribute.cs
+++ ExamPrepTwo70-483/CategoryAttribute.cs
@@ -13,11 +13,33 @@ namespace ExamPrepTwo70_483
         }
     }
 
-   public class UnitTraitAttribute : CategoryAttribute
+    public class UnitTraitAttribute : CategoryAttribute
     {
         public UnitTraitAttribute() : base("Unit Test")
         {
 
+        }
+    }
+
+    //Defining the targets for a custom attribute
+
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Parameter)]
+    public class MyMethodAndParameterAttribute : Attribute { }
+
+    // Setting the AllowMultiple parameter for a custom attribute 
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    class MyMultipleUsageAttribute : Attribute { }
+
+    // Adding properties to a custom attribute
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    class CompleteCustomAttribute : Attribute
+    {
+        public string Description  { get; set; }
+
+        public CompleteCustomAttribute(string description)
+        {
+            Description = description;
         }
     }
 }
